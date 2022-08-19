73c86c5d8b293bcac6f8d05db78f532577f97bf1
Rodrigo Junqueira <oi.rodjun@gmail.com>
2022-08-19 17:19:35 -0300

    Load json data as objects

---
+++ assets/js/Tag.js
@@ -0,0 +1,8 @@
+class Tag {
+
+
+    constructor(aluno_json) {
+        this.ano = aluno_json['ano'];
+        this.cestas_basicas = aluno_json['cestas_basicas_max'];
+    }
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,39 @@
 // SYSTEM
 const SUM_CHECK = 75214; // if JSON file is changed, this needs to be update!!!
 const SIZE = 20;
-let alunos;
+let alunos_json;
 let json_loaded = false;
+let alunos = [];
+
+
+// temp
 let _s = 0;
+
 
 fetch('data/alunos.json')
     .then(response => response.json())
     .then(data => {
-        alunos = _.values(data);
+        alunos_json = _.values(data);
     });
 
 new Q5("global"); // initialize q5js
+
+function preload() {
+    console.log('[boot] pre-loading stuff...')
+}
+
 function setup() {
     createCanvas(windowWidth,windowHeight);
 }
+
 function draw() {
     if (json_loaded) {
         background(200);
         let x = 0;
         let y = 0;
         for (let i = 0; i < alunos.length; i++) {
-            if (alunos[i]['ano'] == 2000) {
-                let c = floor(map(alunos[i]['cestas_basicas_min'],0, 100, 0, 255));
+            if (alunos[i].ano == 2000) {
+                let c = floor(map(alunos[i].cestas_basicas,0, 100, 0, 255));
                 fill(c);
                 rect(x,y,SIZE,SIZE);
                 x += SIZE;
@@ -40,11 +51,17 @@ function draw() {
             if (_s > width) _s = 0;
         }
     } else {
-        if (alunos === undefined || alunos.length != SUM_CHECK) {
-            console.log('[data] loading...');
+        if (alunos_json === undefined || alunos_json.length != SUM_CHECK) {
+            console.log('[boot] loading...');
         } else {
-            console.log('[data] loaded JSON!');
+            console.log('[boot] loaded JSON!');
             json_loaded = true;
+            console.log('[boot] initializing objects...')
+            for (let i = 0; i < alunos_json.length; i++) {
+                alunos.push(new Tag(alunos_json[i]));
+            }
+            alunos_json = null;
+            console.log('[boot] objects initialized!')            
         }
     }
 }
