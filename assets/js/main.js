18286d090484f697c0955e183fba7e27370af539
Rodrigo Junqueira <oi.rodjun@gmail.com>
2022-08-21 19:36:39 -0300

    add navigation between years and zoom capabilities

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,76 @@
-const _GRID_SIZE = 30;
+const _START_SIZE = 30;
+const _STEP_SIZE = 10;
+const _MIN_SIZE = 10;
+
 const _CIRCLE_OPT = .8;
 const _RECT_OPT = .65;
 
+const _START_YEAR = 2000;
+const _MAX_YEAR = 2022;
+const _MIN_YEAR = 2000;
+
+var alunos;
+var size = _START_SIZE;
+var year = _START_YEAR;
+
 d3.json('data/alunos.json').then(data => { // loading data
             
-    const alunos = _.values(data);
+    alunos = _.values(data);
+
+    draw();
+
+});
+
+function zoomIn() {
+    size += _STEP_SIZE;
+    draw();
+}
+
+function zoomOut() {
+    size -= 10;
+    if (size < _MIN_SIZE) {
+        size = _MIN_SIZE;
+    }
+    draw();
+}
+
+function nextYear() {
+    year++;
+    if (year > _MAX_YEAR) {
+        year = _MIN_YEAR
+    }
+    draw();
+}
+
+function prevYear() {
+    year--;
+    if (year < _MIN_YEAR) {
+        year = _MAX_YEAR;
+    }
+    draw();
+}
+
+function draw() {
+    // clear
+    d3.selectAll("#viz > *").remove();
+
+    d3.select('#year').text(year);
 
     // data viz container
     viz = d3.select('#viz');
 
     // create a particle for each row
     viz.selectAll('div')
-    .data(alunos.filter(d => (d.ano === 2002) || (d.ano === 2003)))
+    .data(alunos.filter(d => d.ano === year))
     //.data(alunos)
     .join('div')
     .classed('aluno', true)
-    .style('height', _GRID_SIZE + 'px')
-    .style('width', _GRID_SIZE + 'px')
+    .style('height', size + 'px')
+    .style('width', size + 'px')
     .classed('ensino-particular', d => d.cursou_ensino_medio_publico === 'f')
         .append('svg')
-        .attr('height', _GRID_SIZE)
-        .attr('width', _GRID_SIZE);  
+        .attr('height', size)
+        .attr('width', size);  
 
     // scales
     const colors = d3.scaleOrdinal()
@@ -29,29 +79,28 @@ d3.json('data/alunos.json').then(data => { // loading data
 
     const sizes = d3.scaleLinear()
         .domain([d3.min(alunos, d => d.cestas_basicas), d3.max(alunos, d => d.cestas_basicas)])
-        .range([_GRID_SIZE*.5, _GRID_SIZE]);
+        .range([size*.5, size]);
 
     // draws shapes into each particle
     viz.selectAll('.aluno svg')
         .filter(d => d.sexo === 'm')
         .append('circle')
-        .attr('cx', _GRID_SIZE/2)
-        .attr('cy', _GRID_SIZE/2)
+        .attr('cx', size/2)
+        .attr('cy', size/2)
         .attr('r', d => sizes(d.cestas_basicas)/2*_CIRCLE_OPT )
         .style('fill', d => colors(d.cor_raca_autodeclarada));
 
     viz.selectAll('.aluno svg')
         .filter(d => d.sexo === 'f')
         .append('rect')
-        .attr('x', d => _GRID_SIZE/2-sizes(d.cestas_basicas)*_RECT_OPT/2)
-        .attr('y', d => _GRID_SIZE/2-sizes(d.cestas_basicas)*_RECT_OPT/2)
+        .attr('x', d => size/2-sizes(d.cestas_basicas)*_RECT_OPT/2)
+        .attr('y', d => size/2-sizes(d.cestas_basicas)*_RECT_OPT/2)
         .attr('width', d => sizes(d.cestas_basicas)*_RECT_OPT)
         .attr('height', d => sizes(d.cestas_basicas)*_RECT_OPT)
-        .attr('transform', 'rotate(45,' + _GRID_SIZE/2 + ',' + _GRID_SIZE/2 + ')')
+        .attr('transform', 'rotate(45,' + size/2 + ',' + size/2 + ')')
         .style('fill', d => colors(d.cor_raca_autodeclarada));     
 
     viz.selectAll('.aluno svg')
         .filter(d => d.cor_raca_autodeclarada === 'np')
-        .style('stroke', '#FFFFFF')
-
-});
+        .style('stroke', '#FFFFFF');     
+}
