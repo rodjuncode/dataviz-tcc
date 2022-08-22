861fa42ec43c8c94cae83ecd952cd61e3e3437f0
Rodrigo Junqueira <oi.rodjun@gmail.com>
2022-08-22 10:04:27 -0300

    test svg only structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,9 @@ const _STEP_SIZE = 10;
 const _MIN_SIZE = 15;
 
 const _CIRCLE_OPT = .8;
-const _RECT_OPT = .65;
+const _RECT_OPT = .85;
 
-const _START_YEAR = 2000;
+const _START_YEAR = 2022;
 const _MAX_YEAR = 2022;
 const _MIN_YEAR = 2000;
 
@@ -25,7 +25,7 @@ d3.json('data/alunos.json').then(data => { // loading data
 
     sizes = d3.scaleLinear()
         .domain([d3.min(alunos, d => d.cestas_basicas), d3.max(alunos, d => d.cestas_basicas)])
-        .range([size*.5, size]);
+        .range([.3, .8]);
 
     t = d3.transition()
         .duration(750)
@@ -38,10 +38,15 @@ d3.json('data/alunos.json').then(data => { // loading data
 function zoomIn() {
     // size += _STEP_SIZE;
     // draw();
-    d3.selectAll("#viz > *").transition()
+
+    // d3.selectAll("#viz > *").transition()
+    //     .duration(1500)
+    //     .style('width', '35px')
+    //     .style('height', '35px');
+
+    d3.select('#viz > g').transition()
         .duration(1500)
-        .style('width', '35px')
-        .style('height', '35px');
+        .attr('transform', 'scale(5)');
 
 }
 
@@ -82,42 +87,75 @@ function draw() {
     // data viz container
     viz = d3.select('#viz');
 
-    // create a particle for each row
-    viz.selectAll('div')
-    .data(alunos.filter(d => d.ano === year))
-    //.data(alunos)
-    .join('div')
+   // create a particle for each row
+   viz.append('g').selectAll('circle')
+   .data(alunos.filter(d => d.ano === year))
+   //.data(alunos)
+    .join('circle')
+    .attr('cx', (d,i) => i*_MIN_SIZE)
+    .attr('cy', (d,i) => i*_MIN_SIZE)
+    .attr('r', _MIN_SIZE/2)
     .classed('aluno', true)
-    .style('height', size + 'px')
-    .style('width', size + 'px')
-    .classed('ensino-particular', d => d.cursou_ensino_medio_publico === 'f')
-        .append('svg')
-        .attr('height', '100%')
-        .attr('width', '100%');  
+    .style('fill', d => colors(d.cor_raca_autodeclarada));
+   
+   // draws shapes into each particle
+//    viz.selectAll('.aluno svg')
+//        .filter(d => d.sexo === 'm')
+//        .append('circle')
+//        .attr('cx', '50%')
+//        .attr('cy', '50%')
+//        .attr('r', '50%')
+//        .attr('transform', d => 'scale(' + sizes(d.cestas_basicas) + ')')
+//        .attr('transform-origin', '50% 50%')
+//        .style('fill', d => colors(d.cor_raca_autodeclarada));
 
-    // draws shapes into each particle
-    viz.selectAll('.aluno svg')
-        .filter(d => d.sexo === 'm')
-        .append('circle')
-        // .attr('cx', size/2)
-        // .attr('cy', size/2)
-        .attr('cx', '50%')
-        .attr('cy', '50%')
-        //.attr('r', d => sizes(d.cestas_basicas)/2*_CIRCLE_OPT )
-        .attr('r', '50%')
-        .style('fill', d => colors(d.cor_raca_autodeclarada));
+//    viz.selectAll('.aluno svg')
+//        .filter(d => d.sexo === 'f')
+//        .append('rect')
+//        .attr('width', '100%')
+//        .attr('height', '100%')
+//        .attr('transform', d => 'rotate(45) scale(' + sizes(d.cestas_basicas)*_RECT_OPT + ')')
+//        .attr('transform-origin', '50% 50%')
+//        .style('fill', d => colors(d.cor_raca_autodeclarada));     
 
-    viz.selectAll('.aluno svg')
-        .filter(d => d.sexo === 'f')
-        .append('rect')
-        .attr('x', d => size/2-sizes(d.cestas_basicas)*_RECT_OPT/2)
-        .attr('y', d => size/2-sizes(d.cestas_basicas)*_RECT_OPT/2)
-        .attr('width', d => sizes(d.cestas_basicas)*_RECT_OPT)
-        .attr('height', d => sizes(d.cestas_basicas)*_RECT_OPT)
-        .attr('transform', 'rotate(45,' + size/2 + ',' + size/2 + ')')
-        .style('fill', d => colors(d.cor_raca_autodeclarada));     
+//    viz.selectAll('.aluno svg')
+//        .filter(d => d.cor_raca_autodeclarada === 'np')
+//        .style('stroke', '#FFFFFF');     
 
-    viz.selectAll('.aluno svg')
-        .filter(d => d.cor_raca_autodeclarada === 'np')
-        .style('stroke', '#FFFFFF');     
+    // // create a particle for each row
+    // viz.selectAll('div')
+    // .data(alunos.filter(d => d.ano === year))
+    // //.data(alunos)
+    // .join('div')
+    // .classed('aluno', true)
+    // .style('height', size + 'px')
+    // .style('width', size + 'px')
+    // .classed('ensino-particular', d => d.cursou_ensino_medio_publico === 'f')
+    //     .append('svg')
+    //     .attr('height', '100%')
+    //     .attr('width', '100%');  
+
+    // // draws shapes into each particle
+    // viz.selectAll('.aluno svg')
+    //     .filter(d => d.sexo === 'm')
+    //     .append('circle')
+    //     .attr('cx', '50%')
+    //     .attr('cy', '50%')
+    //     .attr('r', '50%')
+    //     .attr('transform', d => 'scale(' + sizes(d.cestas_basicas) + ')')
+    //     .attr('transform-origin', '50% 50%')
+    //     .style('fill', d => colors(d.cor_raca_autodeclarada));
+
+    // viz.selectAll('.aluno svg')
+    //     .filter(d => d.sexo === 'f')
+    //     .append('rect')
+    //     .attr('width', '100%')
+    //     .attr('height', '100%')
+    //     .attr('transform', d => 'rotate(45) scale(' + sizes(d.cestas_basicas)*_RECT_OPT + ')')
+    //     .attr('transform-origin', '50% 50%')
+    //     .style('fill', d => colors(d.cor_raca_autodeclarada));     
+
+    // viz.selectAll('.aluno svg')
+    //     .filter(d => d.cor_raca_autodeclarada === 'np')
+    //     .style('stroke', '#FFFFFF');     
 }
